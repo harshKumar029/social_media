597c470b340a4615e521278cfd4e9d3f88942edb
harshKumar029 <harshanand029@gmail.com>
2025-01-06 10:14:07 +0530

    change in routs

--- frontend/src/components/Authlayout.jsx
+++ frontend/src/components/Authlayout.jsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import SideBar from './SideBar';
-// import UserList from './UserList';
-
-
-// const AuthLayout = ({ children }) => {
-
-
-
-//     return (
-//         <div className="flex">
-//             <div className="fixed h-full z-[1000]">
-//                 <SideBar />
-//             </div>
-            
-//             <div className="w-[100%] lg:w-[85%] md:w-[78%] ml-auto">
-//                 {children}
-//             </div>
-//             <div className="fixed h-full z-[1000]">
-// <UserList/>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default AuthLayout;
 
 import React from 'react';
 import SideBar from './SideBar';
